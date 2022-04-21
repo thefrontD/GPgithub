1f07f329295645b0c5361adfc00339c3866348c7
thefrontD_desktop <amigojmk@gmail.com>
2022-04-21 16:15:06 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #set ethereum data directory
 fullDataDir="~/ether/data"
+fullDataDir2="~/ether/data2"
 logData="./gethlog.log"
+logData2="./gethlog2.log"
 
 #remove chaindata if you want to start etheruem from genesis block
 rm -vrf ${fullDataDir}
@@ -14,4 +16,7 @@ cd build/bin
 ./geth --datadir ${fullDataDir} init genesis.json
 
 #run geth
-./geth --datadir ${fullDataDir} --keystore "./keystore" --gcmode archive --networkid 76927692 --http --http.port "8081" --http.corsdomain "*" --port 30303 --nodiscover --http.api="admin,eth,debug,miner,net,txpool,personal,web3" --allow-insecure-unlock --rpc console 2>>${logData}
+nohup ./geth --datadir ${fullDataDir} --keystore "./keystore" --gcmode archive --networkid 76927692 --http --http.port "8081" --http.corsdomain "*" --port 30303 --nodiscover --http.api="admin,eth,debug,miner,net,txpool,personal,web3" --allow-insecure-unlock --rpc 2>>${logData} &
+
+nohup ./geth --datadir ${fullDataDi2} --keystore "./keystore" --gcmode archive --networkid 76927692 --http --http.port "8082" --http.corsdomain "*" --port 30303 --nodiscover --http.api="admin,eth,debug,miner,net,txpool,personal,web3" --allow-insecure-unlock --rpc 2>>${logData2} &
+
