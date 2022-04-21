f24727a8428145d01d4642eccf74aa2b7748ee50
thefrontD_desktop <amigojmk@gmail.com>
2022-04-21 13:09:31 +0900

    unzip
    unzip

---
+++ init.sh
@@ -0,0 +1,16 @@
+#set ethereum data directory
+fullDataDir="/ssd/ethereum"
+
+#remove chaindata if you want to start etheruem from genesis block
+rm -vrf ${fullDataDir}
+
+#rebuild geth
+cd ../..
+make geth
+cd build/bin
+
+#init
+./geth --datadir ${fullDataDir} init genesis.json
+
+#run geth
+./geth --datadir ${fullDataDir} --keystore "./keystore" --gcmode archive --networkid 12345 --http --http.port "8081" --http.corsdomain "*" --port 30303 --nodiscover --http.api="admin,eth,debug,miner,net,txpool,personal,web3" --allow-insecure-unlock
---
+++ sendTransaction.py
@@ -0,0 +1,91 @@
+from web3 import Web3
+import sys
+#import socket
+#import random
+#import json
+#import rlp
+#import time
+#import numpy as np
+import os, binascii
+from datetime import datetime
+from multiprocessing import Pool
+
+# Settings
+FULL_PORT = "8081"
+PASSWORD = "1234"
+
+# Account number
+ACCOUNT_NUM = int(sys.argv[1])
+
+# multiprocessing
+THREAD_COUNT = 8
+
+# providers
+fullnode = Web3(Web3.HTTPProvider("http://localhost:" + FULL_PORT))
+
+# functions
+def main():
+    print("Insert ", ACCOUNT_NUM, " accounts")
+
+    # unlock coinbase
+    fullnode.geth.personal.unlockAccount(fullnode.eth.coinbase, PASSWORD, 0)
+
+    # get current block
+    currentBlock = fullnode.eth.blockNumber
+
+    # main loop for send txs
+    print("start sending transactions")
+
+    # send transactions
+    # sendPool.map(sendTransactions, ACCOUNT_NUM)
+    sendTransactions(ACCOUNT_NUM)
+
+    # mining
+    fullnode.geth.miner.start(1)  # start mining
+    while (fullnode.eth.blockNumber == currentBlock):
+        pass # just wait for mining
+    fullnode.geth.miner.stop()  # stop mining
+    currentBlock = fullnode.eth.blockNumber
+
+
+
+def sendTransaction(to):
+    #print("start try to send tx to full node")
+    #print("to: ", to, "/ from: ", fullnode.eth.coinbase)
+    while True:
+        try:
+            fullnode.eth.sendTransaction(
+                {'to': to, 'from': fullnode.eth.coinbase, 'value': '1', 'gas': '21000', 'data': ""})
+            break
+        except:
+            continue
+
+
+
+def sendTransactions(num):
+    for i in range(int(num)):
+        to = makeRandHex()
+        while True:
+            try:
+                fullnode.eth.sendTransaction(
+                    {'to': to, 'from': fullnode.eth.coinbase, 'value': '1', 'gas': '21000', 'data': ""})
+                break
+            except:
+                continue
+
+
+
+def makeRandHex():
+	randHex = binascii.b2a_hex(os.urandom(20))
+	return Web3.toChecksumAddress("0x" + randHex.decode('utf-8'))
+
+
+
+if __name__ == "__main__":
+
+    totalStartTime = datetime.now()
+    sendPool = Pool(THREAD_COUNT) # -> important: this should be in this "__main__" function
+    main()
+    totalEndTime = datetime.now() - totalStartTime
+    print("total elapsed:", totalEndTime.seconds, "seconds")
+    print("DONE")
