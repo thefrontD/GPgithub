6575bdbe7f1c65289b00a996681fe332830ccf18
thefrontD_desktop <amigojmk@gmail.com>
2022-04-21 14:06:35 +0900

    init.sh modify
    add console
    add log

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #set ethereum data directory
 fullDataDir="~/ether/data"
+logData="~/ether/logs/gethlog.log"
 
 #remove chaindata if you want to start etheruem from genesis block
 rm -vrf ${fullDataDir}
@@ -13,4 +14,4 @@ cd build/bin
 ./geth --datadir ${fullDataDir} init genesis.json
 
 #run geth
-./geth --datadir ${fullDataDir} --keystore "./keystore" --gcmode archive --networkid 76927692 --http --http.port "8081" --http.corsdomain "*" --port 30303 --nodiscover --http.api="admin,eth,debug,miner,net,txpool,personal,web3" --allow-insecure-unlock
+./geth --datadir ${fullDataDir} --keystore "./keystore" --gcmode archive --networkid 76927692 --http --http.port "8081" --http.corsdomain "*" --port 30303 --nodiscover --http.api="admin,eth,debug,miner,net,txpool,personal,web3" --allow-insecure-unlock console 2>>${logData}
