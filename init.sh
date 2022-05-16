12e505d042c6652d87279979dab48b6c4278addd
thefrontD_desktop <amigojmk@gmail.com>
2022-05-16 23:34:16 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -20,7 +20,7 @@ cat /dev/null > gethlog2.log
 ./geth --datadir ${fullDataDir} init genesis.json
 
 #run geth
-nohup ./geth --datadir ${fullDataDir} --keystore "./keystore" --gcmode archive --networkid 76927692 --http --http.port "8085" --http.corsdomain "*" --port 30307 --http.api="admin,eth,debug,miner,net,txpool,personal,web3" --allow-insecure-unlock --rpc 2>>${logData} &
+nohup ./geth --datadir ${fullDataDir} --keystore "./keystore" --gcmode archive --networkid 76927692 --http --http.port "8085" --http.corsdomain "*" --port 30307 --http.api="admin,eth,debug,miner,net,txpool,personal,web3" --nodiscover --allow-insecure-unlock --syncmode "full" --rpc 2>>${logData} &
 
 ##export PATH=$PATH:/usr/local/go/bin
 
@@ -28,15 +28,18 @@ nohup ./geth --datadir ${fullDataDir} --keystore "./keystore" --gcmode archive -
 
 ##./geth --datadir "~/ether/data2" init genesis.json
 
-## nohup ./geth --datadir "~/ether/data2" --keystore "./keystore" --gcmode archive --networkid 76927692 --http --http.port "8084" --http.corsdomain "*" --port 30306 --http.api="admin,eth,debug,miner,net,txpool,personal,web3" --allow-insecure-unlock --rpc 2>>"./gethlog2.log" &
+## nohup ./geth --datadir "~/ether/data2" --keystore "./keystore" --gcmode archive --networkid 76927692 --http --http.port "8086" --http.corsdomain "*" --port 30308 --http.api="admin,eth,debug,miner,net,txpool,personal,web3" --nodiscover --allow-insecure-unlock --syncmode "full" --rpc 2>>"./gethlog2.log" &
 
 ##python3 sendTransaction.py 1000
+
+##tail -f -n 60 gethlog.log
 
 ##주영 5.26
 
 ##원본 5.15
 
-
+##removedb를 하고 난 후에는 sync 시간이 길어진다
+##
 
 
 
