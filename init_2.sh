0a2b9455e6a78471ccc455855d2cb2dedf5f861e
thefrontD_desktop <amigojmk@gmail.com>
2022-05-17 18:51:21 +0900

    Update init_2.sh

--- init_2.sh
+++ init_2.sh
@@ -11,4 +11,4 @@ rm -vrf ${fullDataDir}
 cat /dev/null > gethlog2.log
 
 #run geth
-./geth --datadir ${fullDataDir} --keystore "./keystore" --gcmode archive --networkid 12345 --http --http.port "8086" --http.corsdomain "*" --port 30308 --nodiscover --http.api="admin,eth,debug,miner,net,txpool,personal,web3" --allow-insecure-unlock --verbosity "6" --syncmode "full" --rpc 2>>${logData} 
+./geth --datadir ${fullDataDir} --keystore "./keystore" --gcmode archive --networkid 76927692 --http --http.port "8086" --http.corsdomain "*" --port 30308 --nodiscover --http.api="admin,eth,debug,miner,net,txpool,personal,web3" --allow-insecure-unlock --verbosity "6" --syncmode "full" --rpc 2>>${logData} &
